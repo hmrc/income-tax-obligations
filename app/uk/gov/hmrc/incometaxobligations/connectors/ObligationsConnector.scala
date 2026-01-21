ec5e7ab3d24fcfc0706e3454577005b75834ef32
paul-cannings <254883430+paul-cannings@users.noreply.github.com>
2026-01-21 16:04:15 +0000

    MIPR-2343 copied over main base code for obligations

---
+++ app/uk/gov/hmrc/incometaxobligations/connectors/ObligationsConnector.scala
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.incometaxobligations.connectors
+
+import play.api.http.Status.*
+import uk.gov.hmrc.http.client.HttpClientV2
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse, StringContextOps}
+import uk.gov.hmrc.incometaxobligations.config.AppConfig
+import uk.gov.hmrc.incometaxobligations.models.obligations.{ObligationsErrorModel, ObligationsModel, ObligationsResponseModel}
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class ObligationsConnector @Inject()(val http: HttpClientV2,
+                                     val appConfig: AppConfig
+                                    )(implicit ec: ExecutionContext) extends RawResponseReads {
+
+  private[connectors] def getOpenObligationsUrl(nino: String): String = {
+    s"${appConfig.desUrl}/enterprise/obligation-data/nino/$nino/ITSA?status=O"
+  }
+
+  private[connectors] def getAllObligationsDateRangeUrl(nino: String, from: String, to: String): String = {
+    s"${appConfig.desUrl}/enterprise/obligation-data/nino/$nino/ITSA?from=$from&to=$to"
+  }
+
+  def headers: Seq[(String, String)] = appConfig.desAuthHeaders
+
+  private def callObligationsAPI(url: String)(implicit headerCarrier: HeaderCarrier): Future[ObligationsResponseModel] = {
+    http.get(url"$url")
+      .setHeader(headers: _*)
+      .execute[HttpResponse]
+      .map { response =>
+        response.status match {
+          case OK =>
+            logger.info(s"RESPONSE status: ${response.status}, body: ${response.body}")
+            response.json.validate[ObligationsModel](ObligationsModel.desReadsApi1330).fold(
+              invalid => {
+                logger.error(s"Json validation error: $invalid")
+                ObligationsErrorModel(INTERNAL_SERVER_ERROR, "Json Validation Error. Parsing Report Deadlines Data")
+              },
+              valid => {
+                logger.info("successfully parsed response to ObligationsModel")
+                valid
+              }
+            )
+          case _ =>
+            logger.error(s"RESPONSE status: ${response.status}, body: ${response.body}")
+            ObligationsErrorModel(response.status, response.body)
+        }
+      } recover {
+      case ex =>
+        logger.error(s"Unexpected failed future, ${ex.getMessage}")
+        ObligationsErrorModel(INTERNAL_SERVER_ERROR, s"Unexpected failed future, ${ex.getMessage}")
+    }
+  }
+
+  def getOpenObligations(nino: String)
+                        (implicit headerCarrier: HeaderCarrier): Future[ObligationsResponseModel] = {
+    val url = getOpenObligationsUrl(nino)
+
+    logger.info(s"URL - $url ")
+    logger.debug(s"Calling GET $url \n\nHeaders: $headerCarrier \nAuth Headers: ${appConfig.desAuthHeaders}")
+    callObligationsAPI(url)
+  }
+
+  def getAllObligationsWithinDateRange(nino: String, from: String, to: String)
+                                      (implicit headerCarrier: HeaderCarrier): Future[ObligationsResponseModel] = {
+    val url = getAllObligationsDateRangeUrl(nino, from, to)
+
+    logger.info(s"Calling GET $url \n\nHeaders: $headerCarrier \nAuth Headers: ${appConfig.desAuthHeaders}")
+    callObligationsAPI(url)
+  }
+}
---
+++ app/uk/gov/hmrc/incometaxobligations/models/obligations/GroupedObligationsModel.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.incometaxobligations.models.obligations
+
+import play.api.libs.functional.syntax.*
+import play.api.libs.json.{Format, Json, Reads, __}
+
+case class GroupedObligationsModel(identification: String, obligations: Seq[SingleObligationModel])
+
+object GroupedObligationsModel {
+
+  val desReadsApi1330: Reads[GroupedObligationsModel] = (
+
+
+    (__ \\ "identification" \\ "referenceNumber").read[String] and
+      (__ \\ "identification" \\ "incomeSourceType").read[String].flatMap { incomeSourceType =>
+        (__ \\ "obligationDetails").read(
+          Reads.seq(
+            SingleObligationModel.desReadsApi(incomeSourceType)
+          )
+        )
+      }
+    ) (GroupedObligationsModel.apply _)
+
+  implicit val format: Format[GroupedObligationsModel] = Json.format[GroupedObligationsModel]
+}
